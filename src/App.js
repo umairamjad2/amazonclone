dab79360de2ccb28d9a807330fe5ac0b9cae6215
Umair Amjad <umairamjad970@gmail.com>
2025-08-19 12:37:02 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import HomeScreen from "./Screen/HomeScreen/homeScreen";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./Screen/Product/product";
 import Footer from "./components/Footer/footer";
 import Cart from "./Screen/Cart/cart";
@@ -9,15 +9,17 @@ import Cart from "./Screen/Cart/cart";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      <Footer />
-    </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
